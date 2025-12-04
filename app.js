d9e3647dbb9b33f5143b4282d70fae2c8d20ff32
Sakthi Vigneshwari R <sakthi.r1610@gmail.com>
2025-12-04 07:35:01 +0530

    Hardcoded MongoDB Atlas URL for deployment

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const Campground = require("./models/campground");
 //mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 require('dotenv').config();
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yelp-camp';
+console.log("URI for env",process.env.MONGODB_URI);
+//const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yelp-camp';
+const mongoURI = 'mongodb+srv://sakthir1610:December@2025YelpCamp@yelpcamp-cluster.t5twpd9.mongodb.net/yelp-camp?appName=yelpcamp-cluster';
 mongoose.connect(mongoURI);
 
 const db = mongoose.connection;
